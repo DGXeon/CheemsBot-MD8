86994d3d9add21a792e24b034ea5215007fdf4b5
Xeon <88829846+DGXeon@users.noreply.github.com>
2023-07-22 10:22:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = true //auto recording (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
